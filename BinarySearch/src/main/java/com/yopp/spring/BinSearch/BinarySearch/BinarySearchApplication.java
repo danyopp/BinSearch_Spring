1f0ff6d0770c507c8a35eaf7a31c22bfe31a0b49
dan <daniel.yopp@gmail.com>
2021-05-17 17:52:17 -0400

    init commit

---
+++ BinarySearch/src/main/java/com/yopp/spring/BinSearch/BinarySearch/BinarySearchApplication.java
@@ -0,0 +1,13 @@
+package com.yopp.spring.BinSearch.BinarySearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarySearchApplication.class, args);
+	}
+
+}
