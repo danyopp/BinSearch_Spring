847982401aa84a3cc5d21641d7676bea6e2ea1e4
dan <daniel.yopp@gmail.com>
2021-05-20 21:52:27 -0400

    comment update

--- BinarySearch/src/main/java/com/yopp/spring/BinSearch/BinarySearch/SortAlgorithm.java
+++ BinarySearch/src/main/java/com/yopp/spring/BinSearch/BinarySearch/SortAlgorithm.java
@@ -3,6 +3,7 @@ package com.yopp.spring.BinSearch.BinarySearch;
 public interface SortAlgorithm {
     int[] sort(int[] numbers);
 
+    //default swap function to swap to points in array
     default void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
