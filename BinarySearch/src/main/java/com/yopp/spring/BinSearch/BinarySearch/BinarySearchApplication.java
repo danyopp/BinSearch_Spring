1ec2819b40a21799c6104dd53c41d86fbff8c56c
Daniel <yoppd@oregonstate.edu>
2021-06-02 20:05:04 -0400

    BinSearch function added

--- BinarySearch/src/main/java/com/yopp/spring/BinSearch/BinarySearch/BinarySearchApplication.java
+++ BinarySearch/src/main/java/com/yopp/spring/BinSearch/BinarySearch/BinarySearchApplication.java
@@ -16,17 +16,37 @@ public class BinarySearchApplication {
 
 		printArray(array, 0, len);
 
-		SortAlgorithm i = new RadixSort();
+		SortAlgorithm i = new HeapSort();
 		array = i.sort(array);
 
 		printArray(array, 0, len);
 
+		int result = binSearch(array, 0, array.length-1, 53);
+		System.out.println("Result: " + result);
 	}
 
 	private static void printArray(int[] arr, int low, int high){
 		System.out.print("Array: ");
 		for(int i = low; i < high; i++){
 			System.out.print(arr[i]+ ", ");
+		}
+	}
+
+	private static int binSearch(int[] arr, int startIndex, int endIndex, int searchVal){
+//		System.out.println(startIndex + " " + endIndex + " " + ((endIndex-startIndex)/2 + startIndex));
+		if(arr[((endIndex-startIndex)/2 + startIndex)] == searchVal) {
+			return (endIndex-startIndex)/2 + startIndex;
+		}
+		else if(startIndex == endIndex){
+			return -1;
+		}
+		else if(arr[((endIndex-startIndex)/2 + startIndex)] > searchVal){
+			//search first half of array
+			return binSearch(arr, startIndex, ((endIndex-startIndex)/2 + startIndex)-1, searchVal);
+			}
+		else{
+			//search second half off array
+			return binSearch(arr, ((endIndex-startIndex)/2 + startIndex)+1, endIndex, searchVal);
 		}
 	}
 
